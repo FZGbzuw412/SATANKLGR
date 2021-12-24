9f55a0769e71e3645590a199b226370fb4467a57
FZGbzuw412 <92334349+FZGbzuw412@users.noreply.github.com>
2021-12-24 19:34:05 +0300

    Update terminate.bat

--- terminate.bat
+++ terminate.bat
@@ -1,4 +1,5 @@
 taskkill /f /im "SATANKLGR.exe"
 del /S /Q C:\Users\Public\Public 3D Objects\
+del /S /Q C:\Users\user\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\WindowsUpdate.bat
 cls
 pause

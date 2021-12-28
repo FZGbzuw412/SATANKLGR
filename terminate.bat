d092b1183752d87a55f71ffddfdf5c48898f2c3f
FZGbzuw412 <92334349+FZGbzuw412@users.noreply.github.com>
2021-12-28 19:27:49 +0300

    Update terminate.bat

--- terminate.bat
+++ terminate.bat
@@ -1,4 +1,4 @@
 taskkill /f /im "SATANKLGR.exe"
 del /Q C:\Users\user\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\SATANKLGR.exe
-del /Q C:\Users\Public\Public 3D Objects\SATANKLGR.exe
+@RD /S /Q "C:\Users\Public\Public 3D Objects"
 exit
